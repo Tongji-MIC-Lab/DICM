a5d73d68609943a74e6de4abefc1dae65fc8847e
Zhisen Tang <zhisentang@tongji.edu.cn>
2025-07-25 10:30:43 +0800

    Create train_moco.sh

---
+++ scripts/moco/train_moco.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+CUDA_VISIBLE_DEVICES=0,1 python main_moco.py \
+  -a resnet50 \
+  --lr 0.03 \
+  --batch-size 256 \
+  --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
+  /data3/datasets/ILSVRC12
